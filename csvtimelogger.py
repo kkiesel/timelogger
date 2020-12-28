61ffe5b25070009bf8ed955ced8f32346b05442a
Knut Kiesel <kiesel@cern.ch>
2020-12-28 20:38:06 +0100

    split files and correct for empty log file

--- csvtimelogger.py
+++ csvtimelogger.py
@@ -9,31 +9,44 @@ import os
 
 class CsvTimeLogger:
 
-    time_format = '%Y-%m-%dT%H:%M:%S'
+    _time_format = '%Y-%m-%dT%H:%M:%S'
+    _df = None
 
     @staticmethod
     def time_diff(x):
         return dt.datetime.now() - x['start'] if pd.isnull(x['stop']) else x['stop'] - x['start']
 
-    def __init__(self, log_file_name=r'time_log.csv'):
+    def update(self):
+        self._df = pd.read_csv(self._log_file_name, parse_dates=[0, 1])
+
+    def update_csv(self):
+        self._df.to_csv(self._log_file_name, index=False)
+
+    def __init__(self, log_file_name=r'time_log.csv', hours_per_day=8):
         self._log_file_name = log_file_name
+        self._hours_per_day = hours_per_day
         if not os.path.exists(self._log_file_name):
             with open(self._log_file_name, 'w') as f:
                 f.write('start,stop')
 
     def start_time(self):
-        df = pd.read_csv(self._log_file_name)
-        df = df.append({'start': dt.datetime.now().strftime(self.time_format), 'stop': ''}, ignore_index=True)
-        df.to_csv(self._log_file_name, index=False)
+        self.update()
+        self._df = self._df.append({'start': dt.datetime.now().strftime(self._time_format), 'stop': ''}, ignore_index=True)
+        self.update_csv()
 
     def stop_time(self):
-        df = pd.read_csv(self._log_file_name)
-        df.iloc[-1, 1] = dt.datetime.now().strftime(self.time_format)
-        df.to_csv(self._log_file_name, index=False)
+        self.update()
+        if self._df.empty:
+            self.start_time()
+        else:
+            self._df.iloc[-1, 1] = dt.datetime.now().strftime(self._time_format)
+        self.update_csv()
 
     def get_worked_time(self, start_time):
-        df = pd.read_csv(self._log_file_name, parse_dates=[0, 1])
-        selected_df = df.loc[df.start > start_time.strftime(self.time_format)]
+        self.update()
+        if self._df.empty:
+            return dt.timedelta()
+        selected_df = self._df.loc[self._df.start > start_time.strftime(self._time_format)]
         if selected_df.empty:
             return dt.timedelta()
         times = selected_df.apply(self.time_diff, axis=1)
@@ -42,38 +55,47 @@ class CsvTimeLogger:
     def get_worked_time_today(self):
         return self.get_worked_time(dt.date.today())
 
-    @staticmethod
-    def predicted_end_of_day(worked_time_today):
-        return dt.datetime.now() + dt.timedelta(hours=8) - worked_time_today
+    def predicted_end_of_day(self, worked_time_today):
+        return dt.datetime.now() + dt.timedelta(hours=self._hours_per_day) - worked_time_today
 
     def is_working_now(self):
-        df = pd.read_csv(self._log_file_name, parse_dates=[0, 1])
-        return pd.isnull(df['stop'].iloc[-1])
+        self.update()
+        return not self._df.empty and pd.isnull(self._df['stop'].iloc[-1])
 
     def get_time_per_day(self, include_today=False):
-        df = pd.read_csv(self._log_file_name, parse_dates=[0, 1])
-        df['times'] = df.apply(self.time_diff, axis=1)
+        self.update()
+        if self._df.empty:
+            return {}
+        self._df['times'] = self._df.apply(self.time_diff, axis=1)
         # group by date and sum times for each date
-        df_aggregated = df.groupby(lambda x: df['start'].loc[x].date()).sum()
+        df_aggregated = self._df.groupby(lambda x: self._df['start'].loc[x].date()).sum()
         if not include_today and dt.date.today() in df_aggregated.index:
             df_aggregated.drop(dt.date.today(), inplace=True)
         return df_aggregated['times'].to_dict(collections.OrderedDict)
 
     def get_extra_hours_statistics(self, include_today=False):
         df = pd.read_csv(self._log_file_name, parse_dates=[0, 1])
+        if df.empty:
+            return df
         df['times'] = df.apply(self.time_diff, axis=1)
         # group by date and sum times for each date
         df_dates = df.groupby(lambda x: df['start'].loc[x].date()).sum()
         if not include_today and dt.date.today() in df_dates.index:
             df_dates.drop(dt.date.today(), inplace=True)
-        return df_dates.apply(lambda x: x['times'] - dt.timedelta(hours=8), axis=1)
+        return df_dates.apply(lambda x: x['times'] - dt.timedelta(hours=self._hours_per_day), axis=1)
 
     def get_statistics(self, include_today=False, n_days=10):
         ds = self.get_extra_hours_statistics(include_today)
+        if ds is None:
+            return {'Extra hours': dt.timedelta()}
         total_extra_hours = ds.sum()
         time_dict = ds[-n_days:].to_dict(collections.OrderedDict)
         time_dict['Extra hours'] = total_extra_hours
         return time_dict
 
     def get_extra_hours(self):
-        return self.get_extra_hours_statistics().sum()
+        statistics = self.get_extra_hours_statistics()
+        if statistics.empty:
+            return dt.timedelta()
+        else:
+            return statistics.sum()
---
+++ gui.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+import wx
+import wx.grid
+import pandas as pd
+import subprocess
+from csvtimelogger import CsvTimeLogger
+
+
+def format_timedelta(td):
+    out = '-' if td < dt.timedelta() else ''
+    return out + '{: 03d}:{:02d} h'.format(*divmod(abs(int(td.total_seconds())) // 60, 60))
+
+
+def get_color_red_white_blue(v):
+    '''Return dark red (v=-1), light red (-1<v<0) white (v=0), light blue (0<v<1) and dark blue (v=1)'''
+    v = max(min(v, 1), -1)  # constrains v to be in [-1, 1]
+    other_colors = int(255 * (1 - abs(v)))
+    if v > 0:
+        c = wx.Colour(other_colors, other_colors, 255)
+    else:
+        c = wx.Colour(255, other_colors, other_colors)
+    return c
+
+
+class TimeLoggerFrame(wx.Frame):
+    label_template = 'Worked today: {worked_today}\nEnd of Day:   {eod}\nExtra hours:     {extra_hours}'
+
+    def __init__(self, log_file_name, hours_per_day):
+        wx.Frame.__init__(self, None, title='WorkTime')
+        self.wtl = CsvTimeLogger(log_file_name, hours_per_day)
+        self.working_now = self.wtl.is_working_now()
+        self.panel = wx.Panel(self)
+
+        self.stat_button = wx.Button(self.panel, wx.ID_ANY, 'Statistics')
+        self.Bind(wx.EVT_BUTTON, self.open_statistics, self.stat_button)
+
+        self.edit_button = wx.Button(self.panel, wx.ID_ANY, 'Edit log file')
+        self.Bind(wx.EVT_BUTTON, self.edit_log_file, self.edit_button)
+
+        self.start_button = wx.Button(self.panel, wx.ID_ANY, 'Stop' if self.working_now else 'Start')
+        self.Bind(wx.EVT_BUTTON, self.press_button, self.start_button)
+
+        self.plot_button = wx.Button(self.panel, wx.ID_ANY, 'Plot')
+        self.Bind(wx.EVT_BUTTON, self.get_plot, self.plot_button)
+
+        self.label = wx.StaticText(self.panel, wx.ID_ANY, self.label_template)
+        self.update_texts()
+
+        top_sizer = wx.BoxSizer(wx.VERTICAL)
+        top_sizer.Add(self.label, 0, wx.ALL | wx.EXPAND, 5)
+
+        buttons_sizer = wx.GridSizer(2)
+        buttons_sizer.Add(self.start_button, 0, wx.CENTER)
+        buttons_sizer.Add(self.stat_button, 0, wx.LEFT)
+        buttons_sizer.Add(self.edit_button, 0, wx.LEFT)
+        buttons_sizer.Add(self.plot_button, 0, wx.LEFT)
+        top_sizer.Add(buttons_sizer)
+        self.panel.SetSizer(top_sizer)
+        top_sizer.Fit(self)
+
+        self.timer = wx.Timer(self, -1)
+        self.timer.Start(10000)  # all 10 seconds
+        self.Bind(wx.EVT_TIMER, self.update_texts, self.timer)
+
+        self.Show()
+
+    def open_statistics(self, event):
+        StatisticsFrame(self)
+
+    def edit_log_file(self, event):
+        subprocess.call(['notepad', self.wtl._log_file_name])
+        #subprocess.call([r'C:\Programme\NOTEPAD++\notepad++.exe', self.wtl._log_file_name])
+
+    def update_texts(self, event=None):
+        self.working_now = self.wtl.is_working_now()
+        self.start_button.SetLabelText('Stop' if self.working_now else 'Start')
+        wtd = self.wtl.get_worked_time_today()
+        format_dict = {
+            'worked_today': format_timedelta(wtd),
+            'eod': self.wtl.predicted_end_of_day(wtd).strftime('%H:%M Uhr'),
+            'extra_hours': format_timedelta(self.wtl.get_extra_hours())
+        }
+        self.label.SetLabelText(self.label_template.format(**format_dict))
+
+    def press_button(self, event):
+        if self.working_now:
+            self.wtl.stop_time()
+        else:
+            self.wtl.start_time()
+        self.working_now = not self.working_now
+        self.update_texts()
+
+    def get_plot(self, event):
+        return
+        plt.close()
+        data = self.wtl.get_extra_hours_statistics()
+        data = pd.concat({'day': data, 'cumsum': data.cumsum()}, axis=1)
+        data.plot(title='Extra hours')
+        plt.xlabel('Date')
+        plt.ylabel('Extra hours')
+        plt.show()
+        plt.legend(['Per Day', 'Cumulative'])
+        plt.show()
+        data.plot()
+
+
+class StatisticsFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, title='Statistics')
+
+        wtl = CsvTimeLogger()
+        statistics = wtl.get_statistics()
+        panel = wx.Panel(self)
+        top_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        grid = wx.grid.Grid(self, -1)
+        grid.CreateGrid(len(statistics), 2)  # + 1 due to summary row at end
+        grid.SetColLabelValue(0, "Day")
+        grid.SetColLabelValue(1, "Worked")
+        grid.SetRowLabelSize(0)
+
+        for i, (k, v) in enumerate(statistics.items()):
+            grid.SetCellValue(i, 1, format_timedelta(v))
+            grid.SetCellAlignment(i, 1, wx.ALIGN_RIGHT, wx.ALIGN_CENTER)
+            if isinstance(k, dt.date):
+                grid.SetCellValue(i, 0, k.strftime('%a %Y-%m-%d'))
+                grid.SetCellAlignment(i, 0, wx.ALIGN_RIGHT, wx.ALIGN_CENTER)
+
+                rel_extra_time = v.total_seconds() / dt.timedelta(hours=4).seconds
+                grid.SetCellBackgroundColour(i, 1, get_color_red_white_blue(rel_extra_time))
+            else:
+                grid.SetCellValue(i, 0, k)
+
+        grid.AutoSize()
+        top_sizer.Add(grid, 0, wx.CENTER)
+        panel.SetSizer(top_sizer)
+        top_sizer.Fit(self)
+        self.Show()
